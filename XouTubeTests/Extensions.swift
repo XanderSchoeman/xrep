c531e2921c1ea5f93a31845a38de33282a62e655
Xander Schoeman <Xschoeman@jhb.dvt.co.za>
2020-03-09 09:57:36 +0200

    Added first 2 unit tests.

---
+++ XouTubeTests/Extensions.swift
@@ -0,0 +1,49 @@
+//
+//  Extensions.swift
+//  XouTubeTests
+//
+//  Created by Xander Schoeman on 2020/03/09.
+//  Copyright © 2020 Xander Schoeman. All rights reserved.
+//
+public class TestColor {
+    public var testLabel: UIColor
+    init(testlabel: UIColor) {
+        testLabel.self = testlabel
+    }
+}
+
+public class TestLoadImage {
+    public var testImage: UIImageView
+    init(testimage: UIImageView) {
+        testImage.self = testimage
+    }
+}
+
+
+import XCTest
+@testable import XouTube
+@testable import XouDevSpec
+
+class Extensions: XCTestCase {
+    
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    func testRGBExtensionIfItChangesColour(){
+        var testText: UIColor
+        var testText2: UIColor
+        testText = UIColor.rgb(red: 50, green: 50, blue: 50, alpha: 1)
+        testText2 = UIColor.init(red: 50/255, green: 50/255, blue: 50/255, alpha: 1)
+        XCTAssertEqual(testText.cgColor == testText2.cgColor, true)
+    }
+    func testLoadUIImageExtensionIfItLoads() {
+
+    }
+
+    override func tearDown() {
+        testRGBExtensionIfItChangesColour()
+        testLoadUIImageExtensionIfItLoads()
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
